d06e6a7e5426758fc8cd70569eb2ceff4d09d618
Vaelden <maugan.villatel@gmail.com>
2015-08-09 16:23:31 +0100

    Compile capnp files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
-#[test]
-fn it_works() {
+extern crate capnp;
+#[macro_use] extern crate log;
+
+#[allow(dead_code)]
+mod notifications_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schemas/notifications_capnp.rs"));
+}
+
+#[allow(dead_code)]
+mod commands_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schemas/commands_capnp.rs"));
+}
+
+#[allow(dead_code)]
+mod common_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schemas/common_capnp.rs"));
+}
+
+#[allow(dead_code)]
+mod authentication_capnp {
+    include!(concat!(env!("OUT_DIR"), "/schemas/authentication_capnp.rs"));
 }

14bf4f64f3d45ada9cdb9582eaffb10e4b118a33
Vaelden <maugan.villatel@gmail.com>
2015-08-12 22:00:01 +0100

    Cleaned up and reorganized the library

--- src/lib.rs
+++ src/lib.rs
@@ -22,4 +22,119 @@ mod authentication_capnp {
     include!(concat!(env!("OUT_DIR"), "/schemas/authentication_capnp.rs"));
 }
 
-pub mod messages;
+mod serialize;
+pub mod deserialize;
+mod util;
+
+#[derive(Debug,Clone,Copy)]
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[derive(Debug)]
+pub struct EntityOrder {
+    pub entity: u64,
+    pub order: Order,
+}
+
+impl EntityOrder {
+    pub fn new(entity: u64, order: Order) -> EntityOrder {
+        EntityOrder {
+            entity: entity,
+            order: order,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Order {
+    Walk(Option<Direction>),
+    Say(String),
+    // Attack
+    // Cast spell
+    // Talk
+    // Exchange
+    // ...
+}
+
+pub enum MapCommand {
+    Disconnect,
+}
+
+pub enum Command {
+    EntityOrder(EntityOrder),
+    MapCommand(MapCommand),
+}
+
+#[derive(Debug)]
+pub enum Notification {
+    Walk {
+        entity: u64,
+        orientation: Option<Direction>,
+    },
+    Say {
+        entity: u64,
+        message: String,
+    },
+    Location {
+        entity: u64,
+        x: f32,
+        y: f32,
+    },
+    ThisIsYou {
+        entity: u64,
+    },
+}
+
+impl Notification {
+    pub fn walk(id: u64, orientation: Option<Direction>) -> Notification {
+        Notification::Walk {
+            entity: id,
+            orientation: orientation,
+        }
+    }
+
+    pub fn say(id: u64, message: String) -> Notification {
+        Notification::Say {
+            entity: id,
+            message: message,
+        }
+    }
+
+    pub fn location(id: u64, x: f32, y: f32) -> Notification {
+        Notification::Location {
+            entity: id,
+            x: x,
+            y: y,
+        }
+    }
+
+    pub fn this_is_you(id: u64) -> Notification {
+        Notification::ThisIsYou {
+            entity: id,
+        }
+    }
+}
+
+#[derive(Debug,Clone,Copy,Hash,PartialEq,Eq)]
+pub struct AuthenticationToken {
+    data0: u64,
+}
+
+#[derive(Debug,Clone,Copy)]
+pub enum ErrorCode {
+    Success,
+    Error,
+}
+
+// Hack, to be removed later
+pub fn forge_authentication_tokens() -> Vec<AuthenticationToken> {
+    (0..30).map(|i| {
+        AuthenticationToken {
+            data0: i,
+        }
+    }).collect()
+}
--- src/serialize.rs
+++ src/serialize.rs
@@ -4,7 +4,7 @@ use capnp::serialize;
 use capnp::message::{Builder,Allocator};
 use byteorder::{LittleEndian, WriteBytesExt};
 
-use messages::{Order,EntityOrder,Notification,ErrorCode,AuthenticationToken};
+use super::{Order,EntityOrder,Notification,ErrorCode,AuthenticationToken};
 
 use commands_capnp::command::Builder as CommandBuilder;
 use authentication_capnp::authentication_token::Builder as AuthBuilder;
@@ -94,40 +94,3 @@ impl AuthenticationToken {
         serialize_capnp(writer, &builder)
     }
 }
-/*
-pub fn serialize<T: Write>(writer: &mut T, order: &EntityOrder) -> Result<(),Error> {
-    let id = order.target;
-    match order.order {
-        Order::Walk(ref direction) => serialize_walk(writer, id, direction),
-        _ => unimplemented!(),
-    }
-}
-
-fn serialize_walk<T: Write>(writer: &mut T, id: u64, walk: &Option<Direction>) -> Result<(),Error> {
-    let mut message_builder = Builder::new_default();
-    {
-        let mut message = message_builder.init_root::<CommandBuilder>().init_entity_order();
-        message.set_origin(id);
-        message.set_walk(walk.clone().into());
-    }
-    serialize_capnp(writer, &mut message_builder)
-}
-
-// TODO: Make it 256 bits
-pub fn forge_authentication_tokens() -> Vec<AuthenticationToken> {
-    (0..30).map(|i| {
-        AuthenticationToken {
-            data0: i,
-        }
-    }).collect()
-}
-
-pub fn send_authentication_token<T: Write>(writer: &mut T, token: &AuthenticationToken) -> Result<(),Error> {
-    let mut message_builder = Builder::new_default();
-    {
-        let mut message = message_builder.init_root::<AuthBuilder>();
-        message.set_data0(token.data0);
-    }
-    serialize_capnp(writer, &mut message_builder)
-}
-*/
